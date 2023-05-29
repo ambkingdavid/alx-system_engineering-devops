0fe67ba5a0713252898ea6283cf04d947649d97c
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-05-29 09:01:53 +0100

    add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Requesting information from API
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users_url = url + f"users/{sys.argv[1]}"
+    todos_url = url + f"todos"
+
+    users = requests.get(users_url).json()
+    todos = requests.get(todos_url, params={"userId": sys.argv[1]}).json()
+
+    total_task = len(todos)
+    completed = 0
+    titles = []
+    for todo in todos:
+        if todo['completed']:
+            completed += 1
+            titles.append(todo['title'])
+    result = f"Employee {users['name']} is done with tasks({completed}/{total_task})"
+    print(result)
+    for title in titles:
+        print(f"\t{title}")
