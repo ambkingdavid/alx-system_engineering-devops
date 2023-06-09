59853e62f633741a982a96927360b966b4cd347e
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-06-09 18:46:02 +0100

    update file

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 """Function to count words in all hot posts of a given Reddit subreddit."""
+
 import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -38,12 +30,13 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     for c in results.get("children"):
         title = c.get("data").get("title").lower().split()
         for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
+            word_lower = word.lower()
+            if word_lower in title:
+                times = sum(1 for t in title if t == word_lower)
+                if instances.get(word_lower) is None:
+                    instances[word_lower] = times
                 else:
-                    instances[word] += times
+                    instances[word_lower] += times
 
     if after is None:
         if len(instances) == 0:
